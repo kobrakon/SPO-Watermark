0bc209093c74d4110f4b12e8df0336a4fad32ad5
Penn Oser <r1ft4469@users.noreply.github.com>
2022-06-13 23:00:20 -0500

    Added AlphaString Hook

--- SPOWM/Plugin.cs
+++ SPOWM/Plugin.cs
@@ -1,13 +1,18 @@
 ﻿using BepInEx;
+using UnityEngine;
 
 namespace SPOWM
 {
     [BepInPlugin("com.kobrakon.spowatermark", "SPOWatermark", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
+        public static GameObject Hook;
+
         private void Awake()
         {
-            new SPOWMPatch().Enable();
+            Hook = new GameObject("AlphaLabelController");
+            Hook.AddComponent<AlphaLabelController>();
+            DontDestroyOnLoad(Hook);
         }
     }
 }
